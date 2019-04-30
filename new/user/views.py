a0089caa419505592e161c0dba709b8733b5b145
mahidul-islam <mizihan84@gmail.com>
2019-04-30 13:54:59 +0600

    added signup

--- new/user/views.py
+++ new/user/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.template import loader
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+
+def SignUp(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+
+    else:
+        print('method is not Post')
+        form = UserCreationForm()
+
+    template = loader.get_template('registration/signup.html')
+    return HttpResponse(template.render({'form':form}, request))
+
+
+
+# made by me
+# def SignUp(request):
+#     if request.method == 'POST':
+#         print('Method is POST')
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             print('form is valid')
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password1')
+#             user = authenticate(username = username, password = password)
+#             login(request, user)
+#             print(user)
+#             return HttpResponseRedirect(reverse('home'))
+#         else:
+#             template = loader.get_template('registration/signup.html')
+#             context = { 'form':form }
+#             return HttpResponse(template.render(context, request))
+#     else:
+#         form = UserCreationForm()
+#         template = loader.get_template('registration/signup.html')
+#         context = {'form': form}
+#         return HttpResponse(template.render(context, request))
