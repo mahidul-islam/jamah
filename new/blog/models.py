8ec3c5a7af951599bbd8117ee09302951a299e86
mahidul-islam <mizihan84@gmail.com>
2019-05-16 00:50:14 +0600

    Some change in frontend...

--- new/blog/models.py
+++ new/blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+import uuid
 
 
 class Blog(models.Model):
+    uid = models.UUIDField(  default=uuid.uuid4, editable=False)
     heading_text = models.CharField(max_length = 200)
     body_text = models.CharField(max_length = 1000)
     timestamp = models.DateTimeField(default = timezone.now)
@@ -13,3 +15,8 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.heading_text
+
+class VoteForBlog(models.Model):
+    uid = models.UUIDField(  default=uuid.uuid4, editable=False)
+    blog = models.ForeignKey(Blog, on_delete = models.CASCADE)
+    voter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
